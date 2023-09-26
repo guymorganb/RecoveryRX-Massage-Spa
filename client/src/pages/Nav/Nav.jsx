9a2fb38a8fa9f9340e74c189ef5cbd3793712d55
guymorganb <guymorganb@gmail.com>
2023-09-25 20:15:16 -0500

    added user authentication

--- client/src/pages/Nav/Nav.jsx
+++ client/src/pages/Nav/Nav.jsx
@@ -8,7 +8,6 @@ const Nav = ({ onBookNowClick }) => {
   const onHomeRoute = location.pathname === '/' ? true : false;
   const onServicesRoute = location.pathname === '/services' ? true : false;
 
-
   return (
     <Box className="navContainer" width="100%" display="flex" justifyContent="center" >
       <VStack 
@@ -39,6 +38,7 @@ const Nav = ({ onBookNowClick }) => {
             </Text>
           </HStack>
           <HStack spacing={4}>
+
             {location.pathname === '/' && <Flex 
               spacing={4}
               flexDirection={{ base: 'column', sm: 'column', md: "row" }}>
@@ -82,6 +82,19 @@ const Nav = ({ onBookNowClick }) => {
                 boxShadow: '0 2px 3px rgba(0, 0, 0, 0.08), 0 1px 2px rgba(88, 144, 255, 0.1)'}}> 
               <Link color='white.500' href="/reviews" >My Reviews</Link>
             </Box>}
+
+            {onServicesRoute && 
+            <Box
+              as='button' height='40px' lineHeight='1.0'
+              transition='all 0.3s cubic-bezier(0.68, -0.55, 0.27, 1.55)' border='1px' px='18px'
+              borderRadius='8px' fontSize='16px' fontWeight='semibold'
+              bg='#407a10' borderColor='#ccd0d5' color='#ffffff'
+              boxShadow='inset 0px -8px 10px #00000033, inset 0px 4px 4px #ffffff80' _hover={{ 
+                boxShadow: '0 8px 10px rgba(0, 0, 0, 0.14), 0 6px 6px rgba(88, 144, 255, 0.2)'
+              }} _active={{bg: '#edb664', transform: 'scale(0.98)', borderColor: '#bec3c9',
+                boxShadow: '0 2px 3px rgba(0, 0, 0, 0.08), 0 1px 2px rgba(88, 144, 255, 0.1)'}}> 
+              <Link color='white.500' href="/reviews">Home</Link>
+            </Box>}
             
             {onHomeRoute && 
             <Box
@@ -95,6 +108,7 @@ const Nav = ({ onBookNowClick }) => {
                 boxShadow: '0 2px 3px rgba(0, 0, 0, 0.08), 0 1px 2px rgba(88, 144, 255, 0.1)'}}> 
               <Link color='white.500' href="#" onClick={ onBookNowClick }>Book Now</Link>
             </Box>}
+
           </HStack>
         </HStack>
         <Box bg='white.100' width='100%' marginBottom={3}>
